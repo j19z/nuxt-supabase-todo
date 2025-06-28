0fb49317b015640139de878aaca24cf7f887d943
J19Z <jgmusso@gmail.com>
2025-06-28 11:03:19 -0400

    Added small change to test Vercel Deployment

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="max-w-md w-full mx-auto p-4">
       <h1 class="text-2xl font-bold mb-4 text-left">To-Do List</h1>
       <div class="mb-4">
-        <UInput v-model="newTask" placeholder="Enter a new task" class="mr-2" />
+        <UInput v-model="newTask" placeholder="Enter a new task..." class="mr-2" />
         <UButton label="Add Task" color="primary" @click="addTask" />
       </div>
       <div v-for="task in tasks" :key="task.id" class="mb-2 flex items-center">
